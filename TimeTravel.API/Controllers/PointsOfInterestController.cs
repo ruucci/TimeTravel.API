298be567ee4cc1cfd5d0d97f62280520ee6efa28
rdesaideveloper <rdesaideveloper@github.com>
2018-01-11 19:04:56 -0600

    Added basic GET, POST, PUT, PATCH, and DELETE endpoints

--- TimeTravel.API/Controllers/PointsOfInterestController.cs
+++ TimeTravel.API/Controllers/PointsOfInterestController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Linq;
+using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
+using TimeTravel.API.Models;
 
 namespace TimeTravel.API.Controllers
 {
@@ -19,7 +21,7 @@ namespace TimeTravel.API.Controllers
             return Ok(trip.PointsOfInterest);
         }
 
-        [HttpGet("{tripId}/pointsofinterest/{id}")]
+        [HttpGet("{tripId}/pointsofinterest/{id}", Name = "GetPointOfInterest")]
         public IActionResult GetPointOfInterest (int tripId, int id)
         {
             var trip = TripsDataStore.Current.Trips.FirstOrDefault(t => t.Id == tripId);
@@ -38,5 +40,160 @@ namespace TimeTravel.API.Controllers
 
             return Ok(pointsOfInterest);
         }
+
+        [HttpPost("{tripId}/pointsofinterest")]
+        public IActionResult CreatePointOfInterest(int tripId,
+                                                   [FromBody] PointsOfInterestCreatorDto pointOfInterest)
+        {
+            if (pointOfInterest==null)
+            {
+                return BadRequest();
+            }
+
+            if (pointOfInterest.Name.Trim() == pointOfInterest.Description.Trim())
+            {
+                ModelState.AddModelError("Description", "Please enter a description that is different from the name");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var trip = TripsDataStore.Current.Trips.FirstOrDefault(t => t.Id == tripId);
+
+            if (trip == null)
+            {
+                return NotFound();
+            }
+
+            var maxPointOfInterestId = TripsDataStore.Current.Trips.SelectMany(
+                t => trip.PointsOfInterest).Max(p => p.Id);
+
+            var finalPointOfInterest = new PointsOfInterestDto()
+            {
+                Id = ++maxPointOfInterestId,
+                Name = pointOfInterest.Name,
+                Description = pointOfInterest.Description
+            };
+
+            trip.PointsOfInterest.Add(finalPointOfInterest);
+
+            return CreatedAtRoute("GetPointOfInterest", new { tripId = tripId , id = finalPointOfInterest.Id}, finalPointOfInterest);
+        }
+
+        [HttpPut("{tripId}/pointsofinterest/{id}")]
+        public IActionResult UpdatePointOfInterest(int tripId, int id, [FromBody] PointsOfInterestUpdaterDto pointOfInterest)
+        {
+            if (pointOfInterest == null)
+            {
+                return BadRequest();
+            }
+
+            if(pointOfInterest.Name!=null && pointOfInterest.Description!=null)
+            {
+                if (pointOfInterest.Name.Trim() == pointOfInterest.Description.Trim())
+                {
+                    ModelState.AddModelError("Description", "Please enter a description that is different from the name");
+                }
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var trip = TripsDataStore.Current.Trips.FirstOrDefault(t => t.Id == tripId);
+            if (trip == null)
+            {
+                return NotFound();
+            }
+
+            var pointOfInterestFromStore = trip.PointsOfInterest.FirstOrDefault((p => p.Id == id));
+            if(pointOfInterestFromStore==null)
+            {
+                return NotFound();
+            }
+
+            pointOfInterestFromStore.Name = pointOfInterest.Name;
+            pointOfInterestFromStore.Description = pointOfInterest.Description;
+
+            return NoContent();
+        }
+
+        [HttpPatch("{tripId}/pointsofinterest/{id}")]
+        public IActionResult PartialUpdatePointOfInterest(int tripId, int id, [FromBody] JsonPatchDocument<PointsOfInterestUpdaterDto> patchDoc)
+        {
+            if (patchDoc == null)
+            {
+                return BadRequest();
+            }
+
+            var trip = TripsDataStore.Current.Trips.FirstOrDefault(t => t.Id == tripId);
+            if (trip == null)
+            {
+                return NotFound();
+            }
+
+            var pointOfInterestFromStore = trip.PointsOfInterest.FirstOrDefault((p => p.Id == id));
+            if (pointOfInterestFromStore == null)
+            {
+                return NotFound();
+            }
+
+            var pointOfInterestToPatch = 
+                new PointsOfInterestUpdaterDto()
+                {
+                    Name = pointOfInterestFromStore.Name,
+                    Description = pointOfInterestFromStore.Description
+                };
+            patchDoc.ApplyTo(pointOfInterestToPatch, ModelState);
+
+            if (pointOfInterestToPatch.Name != null && pointOfInterestToPatch.Description != null)
+            {
+                if (pointOfInterestToPatch.Name.Trim() == pointOfInterestToPatch.Description.Trim())
+                {
+                    ModelState.AddModelError("Description", "Please enter a description that is different from the name");
+                }
+            }
+
+            if(!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            TryValidateModel(pointOfInterestToPatch);
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            pointOfInterestFromStore.Name = pointOfInterestToPatch.Name;
+            pointOfInterestFromStore.Description = pointOfInterestToPatch.Description;
+
+            return NoContent();
+        }
+
+        [HttpDelete("{tripId}/pointsofinterest/{id}")]
+        public IActionResult DeletePointOfInterest(int tripId, int id)
+        {
+            var trip = TripsDataStore.Current.Trips.FirstOrDefault(t => t.Id == tripId);
+            if (trip == null)
+            {
+                return NotFound();
+            }
+
+            var pointOfInterestFromStore = trip.PointsOfInterest.FirstOrDefault((p => p.Id == id));
+            if (pointOfInterestFromStore == null)
+            {
+                return NotFound();
+            }
+
+            trip.PointsOfInterest.Remove(pointOfInterestFromStore);
+
+            return NoContent();
+        }
+
     }
 }
---
+++ TimeTravel.API/Models/PointsOfInterestCreatorDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace TimeTravel.API.Models
+{
+    public class PointsOfInterestCreatorDto
+    {
+        [Required(ErrorMessage = "Name field is Required")]
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+        [MaxLength(200)]
+        public string Description { get; set; }
+    }
+}
