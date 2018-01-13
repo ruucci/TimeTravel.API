8329bd92ce300a5528d9b50349372718135f2e3b
rdesaideveloper <rdesaideveloper@github.com>
2018-01-13 11:51:54 -0600

    Removed unused code.

--- TimeTravel.API/Program.cs
+++ TimeTravel.API/Program.cs
@@ -2,14 +2,11 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
-using SendGrid;
-using SendGrid.Helpers.Mail;
 
 namespace TimeTravel.API
 {
@@ -24,16 +21,5 @@ namespace TimeTravel.API
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
                 .Build();
-
-        private async Task SendEmail(string email, string subject, string htmlContent)
-        {
-            var apiKey = "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";
-            var client = new SendGridClient(apiKey);
-            var from = new EmailAddress("support@dotnetthoughts.net", "Support");
-            var to = new EmailAddress(email);
-            var plainTextContent = Regex.Replace(htmlContent, "<[^>]*>", "");
-            var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
-            var response = await client.SendEmailAsync(msg);
-        }
     }
 }
